06cde895375e2010e985b7627592d0ca908707e7
Akash98Sky <a98mondal@gmail.com>
2020-11-08 02:44:29 +0530

    sdm660-common: Use power stats mock hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    Signed-off-by: azrim <mirzaspc@gmail.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -333,6 +333,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr
 
 PRODUCT_COPY_FILES += \
