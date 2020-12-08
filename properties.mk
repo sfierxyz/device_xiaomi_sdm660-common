042324c8c752f6bc62efb07d4af2465bd1a8de3f
alanndz <alanmahmud0@gmail.com>
2020-12-08 19:33:58 +0000

    sdm660-common: add ro.lmk.use_psi property to lmkd
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- properties.mk
+++ properties.mk
@@ -151,6 +151,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.lmk.kill_heaviest_task=true \
     ro.lmk.kill_timeout_ms=100 \
     ro.lmk.use_minfree_levels=true
+    ro.lmk.use_psi=true
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
