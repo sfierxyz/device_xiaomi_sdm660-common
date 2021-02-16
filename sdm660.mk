ee4de5dd2669cb243e41e9bd0a56d2802cae4d23
ElXreno <elxreno@gmail.com>
2021-02-16 20:29:33 +0300

    sdm660-common: import XiaomiDoze from https://github.com/LineageOS/android_device_xiaomi_sdm660-common/tree/33048cce9ff8409e7e4adb2500c01631cc739a36
    Signed-off-by: ElXreno <elxreno@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -427,5 +427,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# XiaomiDoze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
