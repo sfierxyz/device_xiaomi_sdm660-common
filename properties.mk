7830f1f0c9d4c31a10aff58e09a3dac76bea1598
DillerOFire <niktofe1@gmail.com>
2021-03-10 20:51:29 +1000

    sdm660-common(props): renable iorapd
    Commit b73d5516136d1a4de39afd74f564daf70b946889 in build/make (rvc-qpr2)
    disabled IORap by default due to a "regression" without mentioning what
    the regression is. Considering how it's been working fine for the past 6
    months and has demonstrated improvements to app launch time [1], let's
    re-enable it here.
    
    [1] https://medium.com/androiddevelopers/improving-app-startup-with-i-o-prefetching-62fbdb9c9020
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>

--- properties.mk
+++ properties.mk
@@ -209,3 +209,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # WFD
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.debug.wfd.enable=0
+
+# IORAP
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.iorapd.enable=true
