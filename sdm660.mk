941b88833f3c8cd18d2b9235ef248bab65a73bdd
DillerOFire <niktofe1@gmail.com>
2021-04-26 12:02:01 +1000

    sdm660-common: targets for wfd

--- sdm660.mk
+++ sdm660.mk
@@ -188,6 +188,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libqdMetaData \
     libqdMetaData.vendor \
+    libqservice \
     libstagefright_enc_common
 
 # Display Hal
@@ -204,8 +205,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9_vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 
 # DPM
