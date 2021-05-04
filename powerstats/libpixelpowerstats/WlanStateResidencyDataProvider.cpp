bd8186face329a4012196337a0fb835d87420bf5
DillerOFire <niktofe1@gmail.com>
2021-05-04 10:43:16 +1000

    sdm660-common: Rebrand libpixelpowerstats to avoid conflicts

--- powerstats/libpixelpowerstats/WlanStateResidencyDataProvider.cpp
+++ powerstats/libpixelpowerstats/WlanStateResidencyDataProvider.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "libpixelpowerstats"
+#define LOG_TAG "libxiaomipowerstats"
 
 #include <android-base/logging.h>
 #include <android-base/properties.h>
